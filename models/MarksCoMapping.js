7277cdc4f5714b31cfa8e4bc141bf503787601a2
Harish23102005 <ganeshanharish2310@gmail.com>
2025-03-23 17:07:34 +0530

    Initial commit for backend

--- models/MarksCoMapping.js
+++ models/MarksCoMapping.js
@@ -12,39 +12,46 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.INTEGER,
         allowNull: false,
       },
-      co_id: {
-        type: DataTypes.INTEGER,
+      coId: {
+        // Change from co_id to coId
+        type: DataTypes.STRING, // Change from INTEGER to STRING
         allowNull: false,
       },
       internal: {
-        // New field
         type: DataTypes.DECIMAL(5, 2),
         allowNull: false,
       },
       exam: {
-        // New field
         type: DataTypes.DECIMAL(5, 2),
         allowNull: false,
       },
       totalInternal: {
-        // New field
         type: DataTypes.DECIMAL(5, 2),
         allowNull: false,
       },
       totalExam: {
-        // New field
         type: DataTypes.DECIMAL(5, 2),
+        allowNull: false,
+      },
+      createdAt: {
+        // Add missing fields
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
         allowNull: false,
       },
     },
     {
-      tableName: "marks_co_mapping",
+      tableName: "marks_co_mappings", // Fix table name to match migration
     }
   );
 
   MarksCoMapping.associate = (models) => {
     MarksCoMapping.belongsTo(models.Mark, { foreignKey: "mark_id" });
-    MarksCoMapping.belongsTo(models.CourseOutcome, { foreignKey: "co_id" });
+    // Remove the CourseOutcome association unless coId references an actual CourseOutcome record
+    // MarksCoMapping.belongsTo(models.CourseOutcome, { foreignKey: "coId" });
   };
 
   return MarksCoMapping;
