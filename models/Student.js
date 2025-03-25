6c06bf70345227d175820794ea608b1ac06493a2
Harish23102005 <ganeshanharish2310@gmail.com>
2025-03-25 16:59:09 +0530

    Fix data structure for frontend and remove duplicate allowed origins

--- models/Student.js
+++ models/Student.js
@@ -1,40 +1,45 @@
 // models/Student.js
-const { DataTypes } = require("sequelize");
-const sequelize = require("../config/database");
+module.exports = (sequelize, DataTypes) => {
+  const Student = sequelize.define(
+    "Student",
+    {
+      student_id: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      department: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      marks: {
+        type: DataTypes.JSONB,
+        allowNull: true,
+      },
+      course_outcomes: {
+        type: DataTypes.JSONB,
+        allowNull: true,
+      },
+      average: {
+        type: DataTypes.FLOAT,
+        allowNull: true,
+      },
+    },
+    {
+      tableName: "students",
+      timestamps: false,
+    }
+  );
 
-const Student = sequelize.define(
-  "Student",
-  {
-    student_id: {
-      type: DataTypes.STRING,
-      primaryKey: true,
-      allowNull: false,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    department: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    marks: {
-      type: DataTypes.JSONB,
-      allowNull: true,
-    },
-    course_outcomes: {
-      type: DataTypes.JSONB,
-      allowNull: true,
-    },
-    average: {
-      type: DataTypes.FLOAT,
-      allowNull: true,
-    },
-  },
-  {
-    tableName: "students",
-    timestamps: false,
-  }
-);
+  // Define associations (if any)
+  Student.associate = (models) => {
+    // Example: Student.hasMany(models.Mark);
+    // Add associations based on your schema
+  };
 
-module.exports = Student;
+  return Student;
+};
